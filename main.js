388a42cd34b2f26d6702099795efd4ff06998290
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-03-10 14:59:58 -0500

    added API form and search

--- main.js
+++ main.js
@@ -1,72 +1,90 @@
 // openLibraryAPI url = "https://openlibrary.org/search.json"
 
-// search API & return first sentence (maybe add author and isbn)
+
+
+
+// search API & display title, author, isbn & first sentence
 async function fetchBookInfo(title) {
-    
+    let titleInput = document.querySelector('#title')
+    let authorInput = document.querySelector('#author')
+    let isbnInput = document.querySelector('#isbn')
+    let apiInput = document.querySelector('#apiInput')
+    // fetch  
     const response = await fetch(`https://openlibrary.org/search.json?title=${title}`);
     const bookInfo = await response.json();
-    // console.log(bookInfo.docs[0].isbn[0])
-    // console.log(bookInfo.docs[0].author_name[0])
-    console.log(bookInfo.docs[0].first_sentence[0]);
-    let newText = bookInfo.docs[0].first_sentence[0]
-    const quote = document.querySelector('.quote')
-    let newQuote = document.createElement('p')
+    // display title
+    let titleTitle = bookInfo.docs[0].title;
+    titleInput.value = `${titleTitle}`;
+    // display author
+    let titleAuthor = bookInfo.docs[0].author_name[0];
+    authorInput.value = `${titleAuthor}`;
+    // display isbn
+    let titleISBN = bookInfo.docs[0].isbn[0];
+    isbnInput.value = `${titleISBN}`;
+    //display first sentence
+    let newText = bookInfo.docs[0].first_sentence[0];
+    const quote = document.querySelector('.quote');
+    let newQuote = document.createElement('p');
     newQuote.innerHTML = `${newText} <td><a href="#" id="delete" class="btn btn-danger btn-sm delete">X</a></td>`
     quote.appendChild(newQuote)
+    // delete first sentence
     let deleteBtns = document.querySelectorAll('#delete')
     for(let button of deleteBtns){
-        button.addEventListener('click', (e) => {
-            e.target.parentElement.parentElement.remove()
-
-        })
+      button.addEventListener('click', (e) => {
+        e.target.parentElement.parentElement.remove()
+        
+      })
     }
-
-    //work on delete
+    // clear apiInput
+    apiInput.value = ""
+    
    //work on catch error 
   }
   
-// Create book
-function bookDetails(title, author, isbn) {
+  
+  
+  // Create book
+  function bookDetails(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
-}
-
-function displayBooks() {
+  }
+  
+  function displayBooks() {
     const books = getBooks();
     books.forEach((book) => addBookToList(book));
   }
-
-// add book to list
+  
+  // add book to list
   function addBookToList(book) {
     const list = document.querySelector("#book-list");
     const row = document.createElement("tr");
-
+  
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
-
+  
     list.appendChild(row);
   }
-
-// clear the input field once book is added
+  
+  // clear the input field once book is added
   function clearInputs() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#isbn').value = '';
   }
-
-// delete book
+  
+  // delete book
   function deleteBook(el) {
     if(el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
-
-// book storage
+  
+  // book storage
   function getBooks() {
     let books;
     if(localStorage.getItem('books') === null) {
@@ -76,13 +94,13 @@ function displayBooks() {
     }
     return books;
   }
-
+  
   function addBook(book) {
     const books = getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
-
+  
   function removeBook(isbn) {
     const books = getBooks();
     books.forEach((book, index) => {
@@ -92,19 +110,28 @@ function displayBooks() {
     });
     localStorage.setItem('books', JSON.stringify(books));
   }
-
-// eventListeners
-// display Book
-document.addEventListener("DOMContentLoaded", displayBooks);
-
-// add a Book
-document.querySelector("#book-form").addEventListener("submit", (e) => {
+  
+  // eventListeners
+  
+  // display Book
+  document.addEventListener("DOMContentLoaded", displayBooks);
+  
+  // search title
+  document.querySelector('#api-form').addEventListener('submit', (e) =>{
+  e.preventDefault();
+  const apiInput = document.querySelector('#apiInput').value;
+  console.log(apiInput)
+  fetchBookInfo(apiInput)
+  })
+  
+  // add book
+  document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const title = document.querySelector("#title").value;
   console.log(title)
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
-
+  
     if (title === ""){
     alert("Please fill in title");
   } else {
@@ -112,14 +139,13 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
     addBookToList(book);
     addBook(book);
     clearInputs();
-    fetchBookInfo(title)
-    
   }
-});
-
-// remove book
-document.querySelector('#book-list').addEventListener('click', (e) => {
+  });
+  
+  // remove book
+  document.querySelector('#book-list').addEventListener('click', (e) => {
   deleteBook(e.target);
   removeBook(e.target.parentElement.previousElementSibling.textContent);
-});
-
+  });
+  
+  
